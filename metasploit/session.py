0078694cb68d1e23312c1ac0c718ba1be8f0a20f
John <coretool@live.com>
2016-12-11 20:38:17 +0100

    first session handling improvements

--- metasploit/session.py
+++ metasploit/session.py
@@ -1,13 +1,61 @@
 class Session(object):
-    def __init__(self, id, type):
-        self.id = id
-        self.type = type
+    def __init__(client, job):
+        infos = client.call('job.info', job)
+        exploit = str(infos[b'name']).replace('Exploit:','')
+        sessions = client.call('session.list')
+        for session in sessions:
+            if exploit in str(sessions[session][b'via_exploit'])
+            self.id = str(session)
+            self.type = str(sessions[session][b'type'])
+            break
+        
+    def write(self, client, command):
+        """
+        Write to a session
+        """
+        if 'meterpreter' in self.type:
+            client.call('session.meterpreter_write', self.id, command + '\n')
+        else:
+            client.call('session.shell_write', self.id, command + '\n')
+            
+    def read(self, client):
+        """
+        Read from a session
+        """
+        if 'meterpreter' in self.type:
+            return client.call('session.meterpreter_read', self.id)[b'result']
+        else:
+            return client.call('session.shell_read', self.id)[b'result']
+        
+    def getsystem(self, client):
+        """
+        Try to raise to SYSTEM level on windows (needs meterpreter)
+        """
+        self.write(client, 'use priv\n')
+        self.write(client, 'getsystem')
+        self.write(client, 'getuid')
+        if 'SYSTEM' in str(self.read(client)):
+            return True
+        return False
+    
+    def raise():
+        pass
 
-    def connect(self, client):
-        client.console_execute('session -i {0}'.format(self.id))
+    def gather():
+        pass
 
-    def execute(self, client, command):
-        client.console_execute(command + '\n')
-
-    def execute_return(self, client, command):
-        return str(client.console_execute(command + '\n')[b'data'])
+    def interactive(self, client):
+        """
+        Run a interactive shell
+        """
+        print('Entering interactive session of type {0}'.format(self.type))
+        while True:
+            command = raw_input('>')
+            # TODO add specific chameleon commands
+            if str.lower(command) == 'exit_interactive':
+                break
+            self.write(client, command)
+            print(self.read(client))
+            
+        print('Exiting interactive session')
+        return False
